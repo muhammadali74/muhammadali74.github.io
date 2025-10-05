fbec5784f8d57e02b4d033a28df6d039dc6c0cec
muhammadali74 <89451260+muhammadali74@users.noreply.github.com>
2025-10-05 10:11:56 +0000

    Deploying to gh-pages from @ muhammadali74/muhammadali74.github.io@fb6e8a12e563aa63c85874cc018504a68146240d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -435,9 +435,9 @@ ninja.data = [{
           description: "Series of Urdu Pretrained Generative Models",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
+            },},{id: "projects-bin-pick-and-place-pincher-x",
+          title: 'Bin Pick and Place | Pincher X',
+          description: "a vision-assisted algorithm pipeline for bin-picking task using PincherX arm.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
             },},{id: "projects-project-3-with-very-long-name",
