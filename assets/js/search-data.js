28f9d8e813907bbb8a4b706967fc69e22b7715bf
muhammadali74 <89451260+muhammadali74@users.noreply.github.com>
2025-12-12 02:05:44 +0000

    Deploying to gh-pages from @ muhammadali74/muhammadali74.github.io@52acd84a138de4887f6e73d9c416414c2f23de7b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -73,6 +73,17 @@ ninja.data = [{
             window.location.href = "/blog/2024/photo-gallery/";
           
         },
+      },{id: "post-a-post-with-images",
+        
+          title: "a post with images",
+        
+        description: "this is what included images could look like",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2024/images/";
+          
+        },
       },{id: "post-google-gemini-updates-flash-1-5-gemma-2-and-project-astra",
         
           title: 'Google Gemini updates: Flash 1.5, Gemma 2 and Project Astra <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
@@ -390,17 +401,6 @@ ninja.data = [{
         handler: () => {
           
             window.location.href = "/blog/2015/code/";
-          
-        },
-      },{id: "post-a-post-with-images",
-        
-          title: "a post with images",
-        
-        description: "this is what included images could look like",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2015/images/";
           
         },
       },{id: "post-a-post-with-formatting-and-links",
